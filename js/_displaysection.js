88d0c95f352e2448c78715d03bfa0416042594e5
KabirKayastha <kayas.kabie@gmail.com>
2019-05-21 14:13:56 +0545

    completed upto Gallery section, responsive left

--- js/_displaysection.js
+++ js/_displaysection.js
@@ -1,20 +1,56 @@
-// $(document).ready(function(){
-//     $('#a-D').click(function(){
-//         $('#right-D').toggle();
-//     });
-// });
+$(document).ready(function(){
+    $('#a-D').click(function(){
+        $('#li-D').addClass('active');
+        $('#li-R').removeClass('active');
+        $('#li-B').removeClass('active');
+        $('#li-O').removeClass('active');
+        $('#li-C').removeClass('active');
+        $('#li-A').removeClass('active');
+    });
 
-function _(id){
-    return document.getElementById(id);
-}
+    $('#a-B').click(function(){
+        $('#li-B').addClass('active');
+        $('#li-R').removeClass('active');
+        $('#li-D').removeClass('active');
+        $('#li-O').removeClass('active');
+        $('#li-C').removeClass('active');
+        $('#li-A').removeClass('active');
+    });
 
-function switchCaret(){
-    if($('#a-D').css('background','red')){
-        $('#right-D').css('display','block');
-    } 
-    else{
-        $('#right-D').css('display','none');
-    }
-}
+    $('#a-R').click(function(){
+        $('#li-R').addClass('active');
+        $('#li-D').removeClass('active');
+        $('#li-B').removeClass('active');
+        $('#li-O').removeClass('active');
+        $('#li-C').removeClass('active');
+        $('#li-A').removeClass('active');
+    });
 
-switchCaret();
+    $('#a-O').click(function(){
+        $('#li-O').addClass('active');
+        $('#li-R').removeClass('active');
+        $('#li-B').removeClass('active');
+        $('#li-D').removeClass('active');
+        $('#li-C').removeClass('active');
+        $('#li-A').removeClass('active');
+    });
+
+    $('#a-C').click(function(){
+        $('#li-C').addClass('active');
+        $('#li-R').removeClass('active');
+        $('#li-B').removeClass('active');
+        $('#li-O').removeClass('active');
+        $('#li-D').removeClass('active');
+        $('#li-A').removeClass('active');
+    });
+
+    $('#a-A').click(function(){
+        $('#li-A').addClass('active');
+        $('#li-R').removeClass('active');
+        $('#li-B').removeClass('active');
+        $('#li-O').removeClass('active');
+        $('#li-C').removeClass('active');
+        $('#li-D').removeClass('active');
+    });
+});
+
