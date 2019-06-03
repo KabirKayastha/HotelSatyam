5b98207c6b841e2ede86b627f5d5b0934d1483bd
KabirKayastha <kayas.kabie@gmail.com>
2019-06-04 01:26:11 +0545

    needs to be finalized

--- js/_starRating.js
+++ js/_starRating.js
@@ -1,63 +1,95 @@
 $(document).ready(function(){
-    
-    // click function
-    // $('#star1').click(function(){
-    //     (this).addClass('checked');
-    // });
 
-    //hover function
-    $('#star1').hover(
+    //click function
+    $('#star1').click(
         function(){
-            $(this).css('color','orange')
-        },
-
-        function(){
-            $(this).css('color','#333')
+            $('#star1').css('color','orange');
         }
     );
 
-    $('#star2').hover(
+    $('#star2').click(
 
         function(){
             $('#star1, #star2').css('color','orange')
-        },
-
-        function(){
-            $('#star1, #star2').css('color','#333');
         }
     );
 
-    $('#star3').hover(
+    $('#star3').click(
 
         function(){
             $('#star1, #star2, #star3').css('color','orange')
-        },
-
-        function(){
-            $('#star1, #star2, #star3').css('color','#333');
         }
     );
 
-    $('#star4').hover(
+    $('#star4').click(
 
         function(){
             $('#star1, #star2, #star3, #star4').css('color','orange')
-        },
-
-        function(){
-            $('#star1, #star2, #star3, #star4').css('color','#333');
         }
     );
 
-    $('#star5').hover(
+    $('#star5').click(
 
         function(){
             $('#star1, #star2, #star3, #star4, #star5').css('color','orange')
-        },
-
-        function(){
-            $('#star1, #star2, #star3, #star4, #star5').css('color','#333');
         }
     );
+
+    //hover function
+    // $('#star1').hover(
+    //     function(){
+    //         $(this).css('color','orange')
+    //     },
+
+    //     function(){
+    //         $(this).css('color','#333')
+    //     }
+    // );
+
+    // $('#star2').hover(
+
+    //     function(){
+    //         $('#star1, #star2').css('color','orange')
+    //     },
+
+    //     function(){
+    //         $('#star1, #star2').css('color','#333');
+    //     }
+    // );
+
+    // $('#star3').hover(
+
+    //     function(){
+    //         $('#star1, #star2, #star3').css('color','orange')
+    //     },
+
+    //     function(){
+    //         $('#star1, #star2, #star3').css('color','#333');
+    //     }
+    // );
+
+    // $('#star4').hover(
+
+    //     function(){
+    //         $('#star1, #star2, #star3, #star4').css('color','orange')
+    //     },
+
+    //     function(){
+    //         $('#star1, #star2, #star3, #star4').css('color','#333');
+    //     }
+    // );
+
+    // $('#star5').hover(
+
+    //     function(){
+    //         $('#star1, #star2, #star3, #star4, #star5').css('color','orange')
+    //     },
+
+    //     function(){
+    //         $('#star1, #star2, #star3, #star4, #star5').css('color','#333');
+    //     }
+    // );
+
+    
 
 });
